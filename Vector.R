eed5048bce00fd6f2e1afe388fe60450c54ff77d
HninPwint <hninpt@gmail.com>
2020-03-17 17:23:07 +0800

    InitialCommit

---
+++ Vector.R
@@ -0,0 +1,18 @@
+getwd()
+list.files()
+(2020-2014)/(2020-2000)
+#2. The population of Australia at the end of 2018 and 2019 was 24.9 and 25.2 million respectively
+# What is the percentage increase in population?
+((25.2 - 24.9)/24.9)*100
+vector_1 <- c(10,20,30,40,50,60,70,80,90,100)
+vector_1 / 2
+vector_1 + 5
+vector_1 + 10
+vector_2 <- c(-10, -20, -30, -40, -50, -60, -70, -80, -90, -100)
+vector_1 + vector_2
+vector_1 * vector_2
+vector_1 / vector_2
+vector_3 <- c(54, 23, 78)
+vector_3
+vector_1 + vector_3
+# longer object length is not a multiple of shorter object length
